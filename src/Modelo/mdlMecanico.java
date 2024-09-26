e8a2fa440b3a7dff14afe6f243894d5be6aac14d
gabyceci <20190019@ricaldone.edu.sv>
2024-09-26 00:19:49 -0600

    .

---
+++ src/Modelo/mdlMecanico.java
@@ -0,0 +1,56 @@
+
+package Modelo;
+
+
+public class mdlMecanico {
+
+    private String uuidMecanico ;
+    private String nombreMecanico ;
+    private Number edadMecanico;
+    private Double pesoMecanico;
+    private String correoMecanico;
+    
+    public String getUuidMecanico() {
+        return uuidMecanico;
+    }
+
+    public void setUuidMecanico(String uuidMecanico) {
+        this.uuidMecanico = uuidMecanico;
+    }
+
+    public String getNombreMecanico() {
+        return nombreMecanico;
+    }
+
+    public void setNombreMecanico(String nombreMecanico) {
+        this.nombreMecanico = nombreMecanico;
+    }
+
+    public Number getEdadMecanico() {
+        return edadMecanico;
+    }
+
+    public void setEdadMecanico(Number edadMecanico) {
+        this.edadMecanico = edadMecanico;
+    }
+
+    public Double getPesoMecanico() {
+        return pesoMecanico;
+    }
+
+    public void setPesoMecanico(Double pesoMecanico) {
+        this.pesoMecanico = pesoMecanico;
+    }
+
+    public String getCorreoMecanico() {
+        return correoMecanico;
+    }
+
+    public void setCorreoMecanico(String correoMecanico) {
+        this.correoMecanico = correoMecanico;
+    }
+    
+    
+    
+    
+}
